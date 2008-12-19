8aebd08c1610b3ff05e0804465e6cd388e9d35e8
alex <alexrabarts@gmail.com>
2008-12-19 13:47:15 +1100

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,30 @@
+# newWindow: a jQuery plugin
+
+newWindow is a super-simple jQuery plugin for opening new browser windows
+when a anchor is clicked.
+
+## Usage
+
+Open all links with a <code>rel</code> attribute of <code>external</code>
+in a new window:
+
+  <pre>
+    $('a[rel~=external]').newWindow();
+  </pre>
+
+Add a callback when a new window is opened:
+
+  <pre>
+    $('#myLink').newWindow({
+      open: function (newWindow, e) {
+        console.log('Window opened', newWindow, e);
+      }
+    });
+  </pre>
+
+# Licensing
+
+Licensed under the MIT:
+http://www.opensource.org/licenses/mit-license.php
+
+Copyright (c) 2008 Stateless Systems (http://statelesssystems.com)
---
+++ jquery.newwindow.js
@@ -0,0 +1,35 @@
+/*
+ * newWindow: a jQuery plugin
+ *
+ * newWindow is a super-simple jQuery plugin for opening new browser windows
+ * when a anchor is clicked.
+ *
+ * For usage and examples, visit:
+ * http://github.com/alexrabarts/jquery-newwindow
+ *
+ * Licensed under the MIT:
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * Copyright (c) 2008 Stateless Systems (http://statelesssystems.com)
+ *
+ * @author   Alex Rabarts (alexrabarts -at- gmail -dawt- com)
+ * @requires jQuery v1.2 or later
+ * @version  0.1
+ */
+
+(function ($) {
+  $.extend($.fn, {
+    newWindow: function (options) {
+      var defaults = {open: function () {}};
+      options = $.extend(defaults, options || {});
+
+      return this.each(function () {
+        $(this).click(function (e) {
+          e.preventDefault();
+          var newWindow = open(this.href);
+          options.open.call(newWindow, e);
+        });
+      });
+    }
+  });
+})(jQuery);
